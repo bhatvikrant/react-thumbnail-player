a2ce4a714ff302a722982b01a73edddfd5f612ae
Vikrant Bhat <vik.bhat223@gmail.com>
2020-03-16 20:56:17 +0530

    example updated

--- example/src/App.js
+++ example/src/App.js
@@ -1,24 +1,82 @@
 import React, { Component } from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 
 import VideoThumbnail from 'react-thumbnail-player'
 
 export default class App extends Component {
 	render() {
 		return (
-			<div>
-				<VideoThumbnail
-					title='hello yt'
-					message='hello this is a msg'
-					preview='http://clips.vorwaerts-gmbh.de/VfE_html5.mp4'
-					width={350}
-					muted={true}
-					badge='Live'
-					badgeBg='blue'
-					classname='customClassName'
-				/>
-
-				<VideoThumbnail />
-			</div>
+			<Container>
+				<br />
+				<br />
+				<br />
+				<br />
+				<br />
+				<br />
+				<br />
+				<br />
+				<br />
+				<Row>
+					<Col>
+						<h1 className='text-center'>react-thumbnail-player DEMO</h1>
+					</Col>
+				</Row>
+				<Row>
+					<Col>
+						<h6 className='text-center'>
+							<a href='https://bhatvikrant.github.io/react-thumbnail-player/'>
+								Visit the Documentation
+							</a>
+						</h6>
+					</Col>
+				</Row>
+				<br />
+				<br />
+				<br />
+				<Row>
+					<Col lg={6}>
+						<VideoThumbnail
+							title='Test video'
+							message='Add a message / body here'
+							preview='http://clips.vorwaerts-gmbh.de/VfE_html5.mp4'
+							width={350}
+							muted={true}
+							badge='Recorded'
+							badgeBg='red'
+							classname='customClassName'
+						/>
+						<br />
+						<h6 className='text-center'>
+							Video plays on hover{' '}
+							<b>
+								without Sound
+								<span role='img' aria-label=''>
+									🔕
+								</span>
+							</b>
+						</h6>
+						<br />
+						<br />
+						<br />
+					</Col>
+					<Col lg={6}>
+						<VideoThumbnail />
+						<br />
+						<h6 className='text-center'>
+							Video plays on hover{' '}
+							<b>
+								with Sound
+								<span role='img' aria-label=''>
+									🔔
+								</span>
+							</b>
+						</h6>
+						<br />
+						<br />
+						<br />
+					</Col>
+				</Row>
+			</Container>
 		)
 	}
 }
