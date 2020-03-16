8572de885cf9f8cfa029a5076fe1283392a5ff61
Vikrant Bhat <vik.bhat223@gmail.com>
2020-03-16 19:56:18 +0530

    first commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,24 +1,24 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
-import VideoThumbnail from "reactautoplay";
+import VideoThumbnail from 'react-thumbnail-player'
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <VideoThumbnail
-          title="hello yt"
-          message="hello this is a msg"
-          preview="http://clips.vorwaerts-gmbh.de/VfE_html5.mp4"
-          width={350}
-          muted={true}
-          badge="Live"
-          badgeBg="blue"
-          classname="customClassName"
-        />
+	render() {
+		return (
+			<div>
+				<VideoThumbnail
+					title='hello yt'
+					message='hello this is a msg'
+					preview='http://clips.vorwaerts-gmbh.de/VfE_html5.mp4'
+					width={350}
+					muted={true}
+					badge='Live'
+					badgeBg='blue'
+					classname='customClassName'
+				/>
 
-        <VideoThumbnail />
-      </div>
-    );
-  }
+				<VideoThumbnail />
+			</div>
+		)
+	}
 }
